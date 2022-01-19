70709c8f59ef4e6ee88059f39aa7eee2c958d86c
Farhan Shaikh <blaxerf@gmail.com>
2022-01-19 18:45:50 +0530

    Added the feature the Share application through Google Drive link.

--- app/src/main/java/com/example/vstream/Dashboard.java
+++ app/src/main/java/com/example/vstream/Dashboard.java
@@ -344,9 +344,24 @@ public class Dashboard extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.settings_app:
                 startActivity(new Intent(getApplicationContext(),SettingActivity.class));
+
+            case R.id.share_app:
+                shareApplication();
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    //Update the application hosted link when the new version is available
+    private void shareApplication() {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT,
+                "Hey check out my app at: https://drive.google.com/file/d/1Hzl6RwWJNVLXzCs5UBYhsVMNDnmCT5w6/view?usp=sharing");
+                sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, "Choose one"));
+    }
+
 
     //Checking if the application is connected to the internet or not
     boolean status;
