f634b7d21468da624d016017a6b2be678a08928b
Farhan Shaikh <blaxerf@gmail.com>
2022-01-23 17:23:07 +0530

    Added Animation to Splash Screen Activity and About App Activity.

--- app/src/main/java/com/example/vstream/AboutApp.java
+++ app/src/main/java/com/example/vstream/AboutApp.java
@@ -6,8 +6,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class AboutApp extends AppCompatActivity {
+
+    Animation topAnim,bottomAnim;
+    TextView aboutText;
+    TextView devName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,20 @@ public class AboutApp extends AppCompatActivity {
         ColorDrawable colorDrawable
                 = new ColorDrawable(Color.parseColor("#3700B3"));
         actionBar.setBackgroundDrawable(colorDrawable);
+
+        //Initializing the animation
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Typecasting
+        aboutText = (TextView) findViewById(R.id.about_app_tv);
+        devName = (TextView) findViewById(R.id.dev_name);
+
+        //Hooking the animation
+        aboutText.setAnimation(topAnim);
+        devName.setAnimation(bottomAnim);
+
+
 
     }
 }
--- app/src/main/java/com/example/vstream/Splash.java
+++ app/src/main/java/com/example/vstream/Splash.java
@@ -8,14 +8,38 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+
+    Animation topAnim,bottomAnim;
+    TextView banner,slogan;
+    ImageView logo;
 
     @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Initializing the animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //Typecasting
+        logo = (ImageView)findViewById(R.id.Vstream_logo);
+        banner = (TextView) findViewById(R.id.Vstream_banner);
+        slogan = (TextView)findViewById(R.id.Vstream_slogan);
+
+        //Hooking the animations
+        logo.setAnimation(topAnim);
+        banner.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+
         //Hide Notification Panel
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
